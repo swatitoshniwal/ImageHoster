598d4031a5fc2ed87b5b7dabade1b18555834bac
swatitoshniwal <swati.toshniwal2@gmail.com>
2020-03-16 22:03:14 +0530

    Complete commit of Image Hoster

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,77 @@
+package ImageHoster.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table (name= "comments")
+public class Comment {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "createdDate")
+    private LocalDate createdDate;
+
+    //The 'comments' table is mapped to 'users' table with Many:One mapping
+    //One comment can have only one user (owner) but one user can have multiple comments
+    //FetchType is EAGER
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'comments' table referring the primary key in 'users' table will be 'user_id'
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    //The 'comments' table is mapped to 'image' table with Many:One mapping
+    //One comment can have only one image but one image can have multiple comments
+    //FetchType is EAGER
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'comments' table referring the primary key in 'images' table will be 'image_id'
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
